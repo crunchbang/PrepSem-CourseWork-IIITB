5c8a9ecc6b375888b15c10e511b1fef420472c81
Athul Suresh <athulsure@gmail.com>
2016-07-18 01:02:48 +0530

    Add edit methods

--- 2016-Java_Programming/workspace/SpellChecker22/src/CorpusAnalyzer.java
+++ 2016-Java_Programming/workspace/SpellChecker22/src/CorpusAnalyzer.java
@@ -1,42 +1,50 @@
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class CorpusAnalyzer {
-	
-	private static HashMap<String, Integer> wordDict = new HashMap<String, Integer>();
-	private PrintWriter out; 
 
+	private static char[] charArray = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	private HashMap<String, Integer> wordDict; 
+	private PrintWriter out; 
+	private ArrayList<Pair> partitions;
+	private Set<String> suggestions;
+
+	public CorpusAnalyzer () {
+
+		wordDict = new HashMap<String, Integer>();
+		partitions = new ArrayList<Pair>();
+		suggestions = new HashSet<String>();
+	}
 
 	public void createWordDict(String fileName) throws IOException {
 
 		//read the file
 		FileReader f = new FileReader(fileName);
 		BufferedReader br = new BufferedReader(f);
-		out = new PrintWriter(new BufferedOutputStream(System.out));
 		// read a line
 		String line;
 		String regEx = "\\P{Alpha}+";
 		while ((line = br.readLine()) != null) {
-			//out.println(line); // DEBUG
 			String[] tokens = line.toLowerCase().split(regEx, -1);
 			//tokenize the input into word
 			//ignore non-alphabet symbols
 			for (String word : tokens) {
-				//out.println(word + "|"); //DEBUG
 				//ugly hack
 				if (word.equals(""))
 					continue;
+				//store into a hash map <word, no_of_occurrences>
 				int freq = wordDict.containsKey(word) ? wordDict.get(word) : 0;
 				wordDict.put(word, ++freq);
 			}
 		}
-		showDict(); //DEBUG
-		//store into a hash map <word, no_of_occurrences>
-		out.close();
+		br.close();
 	}
 
 	public void showDict() {
@@ -45,24 +53,117 @@ public class CorpusAnalyzer {
 			System.out.println(item + " " + val);
 		}
 	}
-	
-	public void edits()
-	{
-		
-	}
-	
-	void transpose() {
-		
-	}
-	void inserts() {
-		
-	}
-	void deletes() {
-		
-	}
-	void replace() {
-		
-	}
-	
 
+	public void showPartitions() {
+		Iterator<Pair> itr = partitions.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+
+	public void edits(String word) {
+
+		String fst, snd;
+		for (int i = 1; i < word.length(); ++i) {
+			fst = word.substring(0, i);
+			snd = word.substring(i, word.length());
+			if (fst != null && snd != null) 
+				partitions.add(new Pair(fst, snd));
+		}
+
+		suggestions.add(word);
+		suggestions.addAll(inserts());
+		suggestions.addAll(deletes());
+		suggestions.addAll(transpose());
+		suggestions.addAll(replace());
+
+	}
+
+	public ArrayList<String> inserts() {
+
+		ArrayList<String> insertSuggestions = new ArrayList<String>();
+		for (Pair p : partitions) {
+			for (char c : charArray) {
+				String s = p.getFirst() + c + p.getSecond();
+				if (wordDict.containsKey(s)) {
+					insertSuggestions.add(s);
+				}
+			}
+		}
+		System.out.println("in" + insertSuggestions);
+		return insertSuggestions;
+	}
+
+	public ArrayList<String> deletes() {
+
+		ArrayList<String> deleteSuggestions = new ArrayList<String>();
+		for (Pair p : partitions) {
+			String s = p.getFirst() + p.getSecond().substring(1);
+			if (wordDict.containsKey(s)) {
+				deleteSuggestions.add(s);
+			}
+		}
+		System.out.println("del" + deleteSuggestions);
+		return deleteSuggestions;
+	}
+
+	public ArrayList<String> replace() {
+
+		ArrayList<String> replaceSuggestions = new ArrayList<String>();
+		for (Pair p : partitions) {
+			for (char c : charArray) {
+				String s = p.getFirst() + c + p.getSecond().substring(1);
+				if (wordDict.containsKey(s)) {
+					replaceSuggestions.add(s);
+				}
+			}
+		}
+		System.out.println("re" + replaceSuggestions);
+		return replaceSuggestions;
+	}
+
+	public ArrayList<String> transpose() {
+
+		ArrayList<String> transposeSuggestions = new ArrayList<String>();
+		for (Pair p : partitions) {
+			String fst = p.getFirst();
+			String snd = p.getSecond();
+			if (snd.length() > 2) {
+				String s = fst + snd.charAt(1) + snd.charAt(0) + snd.substring(2);
+				if (wordDict.containsKey(s)) {
+					transposeSuggestions.add(s);
+				}
+			}
+		}
+		System.out.println("tr:" + transposeSuggestions);
+		return transposeSuggestions;
+	}
+
+}
+
+class Pair {
+	String first;
+	String second;
+
+	public Pair(String first, String second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getSecond() {
+		return second;
+	}
+	public void setSecond(String second) {
+		this.second = second;
+	}
+
+	public String toString() {
+		return first + " " + second;
+	}
 }
--- 2016-Java_Programming/workspace/SpellChecker22/src/SpellChecker22.java
+++ 2016-Java_Programming/workspace/SpellChecker22/src/SpellChecker22.java
@@ -1,19 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class SpellChecker22 {
-	
+
 	public static void main(String[] args) {
-		
+
 		try {
 			CorpusAnalyzer c = new CorpusAnalyzer();
 			c.createWordDict("big_doc.txt");
-			//System.out.println("Done");
+			System.out.println("Done");
+
+			//feed input
+			FileReader f = new FileReader("test1.txt");
+			BufferedReader inp = new BufferedReader(f);
+			String line;
+			while ((line = inp.readLine()) != null) {
+				String[] input = line.split("\\s+", -1);
+				c.edits(input[0]);
+			}
+			//c.showPartitions();
+			inp.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Done");
 			e.printStackTrace();
 		}
-		//c.showDict();
-		
+
 	}
 
+
 }
+
